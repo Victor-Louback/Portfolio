1c53cac5117dd01812552104fec50ca420ccd7ad
Carlos Victor Louback <115109126+Victor-Louback@users.noreply.github.com>
2023-08-26 17:30:34 -0300

    atualização maquina de escrever

--- script.js
+++ script.js
@@ -9,21 +9,13 @@ function typeWriter(elemento, tempo) {
 function startTypingAnimation() {
     const titulo1 = document.getElementById('titulo1');
     const titulo2 = document.getElementById('titulo2');
-    titulo2.classList.add('invisivel');
 
-    typeWriter(titulo1, 80);
+    typeWriter(titulo1, 100);
     setTimeout(() => {
-        titulo2.classList.remove('invisivel'); typeWriter(titulo2, 80); // Chama typeWriter para o titulo2 após um atraso de 3 segundos
-    }, 2000);
+        titulo2.classList.remove('invisivel'); typeWriter(titulo2, 100); // Chama typeWriter para o titulo2 após um atraso de 3 segundos
+    }, 2500);
     
 }
 
 // Inicializar a animação de digitação
 startTypingAnimation();
-
-// Definir um intervalo para reiniciar a animação após um certo período
-const intervalo = 10000; // 7 segundos
-
-setInterval(() => {
-    startTypingAnimation();
-}, intervalo);
